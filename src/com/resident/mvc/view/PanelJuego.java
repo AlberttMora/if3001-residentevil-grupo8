f7fcd92bc0103dc689aaab060a3b236029149895
diego <diego@DiegoVC>
2026-06-19 16:46:34 -0600

    viewPricipal y PanelJuego

---
+++ src/com/resident/mvc/view/PanelJuego.java
@@ -0,0 +1,113 @@
+package com.resident.mvc.view;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridLayout;
+
+public class PanelJuego extends JPanel {
+	private JPanel btnPuertaIzq;
+	private JPanel btnPuertaDer;
+	private JLabel btnSalir;
+	private JLabel btnSiguiente;
+	private JPanel pnlNotasEncontradas;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelJuego() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		btnPuertaIzq = new JPanel();
+		btnPuertaIzq.setOpaque(false);
+		btnPuertaIzq.setIgnoreRepaint(true);
+		btnPuertaIzq.setBounds(396, 133, 151, 331);
+		panel.add(btnPuertaIzq);
+		btnPuertaIzq.setLayout(null);
+		
+		btnPuertaDer = new JPanel();
+		btnPuertaDer.setLayout(null);
+		btnPuertaDer.setOpaque(false);
+		btnPuertaDer.setIgnoreRepaint(true);
+		btnPuertaDer.setBounds(654, 133, 151, 331);
+		panel.add(btnPuertaDer);
+		
+		pnlNotasEncontradas = new JPanel();
+		pnlNotasEncontradas.setOpaque(false);
+		pnlNotasEncontradas.setBounds(810, 510, 330, 55);
+		panel.add(pnlNotasEncontradas);
+		pnlNotasEncontradas.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		btnSalir = new JLabel("");
+		btnSalir.setIcon(new ImageIcon(PanelJuego.class.getResource("/resources/img/botonSalir.fw.png")));
+		btnSalir.setBounds(0, 0, 259, 112);
+		panel.add(btnSalir);
+		
+		JLabel imgVidas_Notas = new JLabel("");
+		imgVidas_Notas.setIcon(new ImageIcon(PanelJuego.class.getResource("/resources/img/Vidas_Notas.fw.png")));
+		imgVidas_Notas.setBounds(0, 488, 259, 112);
+		panel.add(imgVidas_Notas);
+		
+		JLabel imgVerNotas = new JLabel("");
+		imgVerNotas.setBounds(732, 466, 468, 145);
+		panel.add(imgVerNotas);
+		imgVerNotas.setIcon(new ImageIcon(PanelJuego.class.getResource("/resources/img/verNotas.fw.png")));
+		
+		btnSiguiente = new JLabel("");
+		btnSiguiente.setIcon(new ImageIcon(PanelJuego.class.getResource("/resources/img/botonSiguiente.fw.png")));
+		btnSiguiente.setBounds(905, -23, 295, 135);
+		panel.add(btnSiguiente);
+		
+		JLabel imgP_Juego = new JLabel("");
+		imgP_Juego.setIcon(new ImageIcon(PanelJuego.class.getResource("/resources/img/PantallaJuego.fw.png")));
+		imgP_Juego.setBounds(0, 0, 1200, 600);
+		panel.add(imgP_Juego);
+	}
+
+	public JPanel getBtnPuertaIzq() {
+		return btnPuertaIzq;
+	}
+
+	public void setBtnPuertaIzq(JPanel btnPuertaIzq) {
+		this.btnPuertaIzq = btnPuertaIzq;
+	}
+
+	public JPanel getBtnPuertaDer() {
+		return btnPuertaDer;
+	}
+
+	public void setBtnPuertaDer(JPanel btnPuertaDer) {
+		this.btnPuertaDer = btnPuertaDer;
+	}
+
+	public JLabel getBtnSalir() {
+		return btnSalir;
+	}
+
+	public void setBtnSalir(JLabel btnSalir) {
+		this.btnSalir = btnSalir;
+	}
+
+	public JLabel getBtnSiguiente() {
+		return btnSiguiente;
+	}
+
+	public void setBtnSiguiente(JLabel btnSiguiente) {
+		this.btnSiguiente = btnSiguiente;
+	}
+
+	public JPanel getPnlNotasEncontradas() {
+		return pnlNotasEncontradas;
+	}
+
+	public void setPnlNotasEncontradas(JPanel pnlNotasEncontradas) {
+		this.pnlNotasEncontradas = pnlNotasEncontradas;
+	}
+}
---
+++ src/com/resident/mvc/view/ViewPrincipal.java
@@ -0,0 +1,77 @@
+package com.resident.mvc.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class ViewPrincipal extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel btnJugar;
+	private JLabel btnSalir;
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewPrincipal() {
+		setMaximumSize(new Dimension(1200, 600));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1200, 600);
+		contentPane = new JPanel();
+		contentPane.setMaximumSize(new Dimension(1200, 600));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panel = new JPanel();
+		panel.setMaximumSize(new Dimension(1200, 600));
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+
+		btnJugar = new JLabel("");
+		btnJugar.setIcon(new ImageIcon(ViewPrincipal.class.getResource("/resources/img/botonJugar.fw.png")));
+		btnJugar.setBounds(425, 390, 259, 112);
+		panel.add(btnJugar);
+
+		btnSalir = new JLabel("");
+		btnSalir.setIcon(new ImageIcon(ViewPrincipal.class.getResource("/resources/img/botonSalir.fw.png")));
+		btnSalir.setBounds(0, 0, 259, 112);
+		panel.add(btnSalir);
+
+		JLabel imgP_Inicio = new JLabel("");
+		imgP_Inicio.setIcon(new ImageIcon(ViewPrincipal.class.getResource("/resources/img/pantallaInicio.fw.png")));
+		imgP_Inicio.setPreferredSize(new Dimension(1200, 600));
+		imgP_Inicio.setBounds(0, 0, 1172, 543);
+		panel.add(imgP_Inicio);
+	}
+	public JLabel getBtnJugar() {
+		return btnJugar;
+	}
+
+
+	public void setBtnJugar(JLabel btnJugar) {
+		this.btnJugar = btnJugar;
+	}
+
+
+	public JLabel getBtnSalir() {
+		return btnSalir;
+	}
+
+
+	public void setBtnSalir(JLabel btnSalir) {
+		this.btnSalir = btnSalir;
+	}
+
+	public void init() {
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
